1893d5cc9366b697587c8c60f8b0376a7ab0f35a
ARMAAN <258284991+armaan-syed@users.noreply.github.com>
2026-02-05 21:45:39 +0530

    2nd commit
    updated the code . debigged and tested

--- sendmail.py
+++ sendmail.py
@@ -1,18 +1,49 @@
-import numpy as np
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-import matplotlib.pyplot as plt
+try:
+    #taking senders info
+    sender_email = input("Enter your Gmail address: ").strip()
+    app_password = input("Enter your Gmail App Password: ").strip()
 
-# Create data points
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
+    # taking recievers email
+    emails_input = input("Enter recipient emails separated by commas:\n")
+    
+    # Convert'ing string -> list
+    receivers = [email.strip() for email in emails_input.split(",") if email.strip()]
 
-# Create the plot
-plt.plot(x, y, 'b-', label='sine wave')
-plt.title('Simple Sine Wave Plot')
-plt.xlabel('X axis')
-plt.ylabel('Y axis')
-plt.grid(True)
-plt.legend()
+    if not receivers:
+        print("No valid email addresses entered.")
+        exit()
 
-# Display the plot
-plt.show()
+    # taking subject
+    subject = input("Enter subject: ")
+    body = input("Enter message: ")
+
+    # creating email
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["Subject"] = subject
+    message.attach(MIMEText(body, "plain"))
+
+    # SMPT
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(sender_email, app_password)
+
+    # sending the mails
+    server.sendmail(sender_email, receivers, message.as_string())
+
+    print("\n Email sent successfully to:")
+    for email in receivers:
+        print("✔", email)
+
+except Exception as e:
+    print("\n Error occurred:", e)
+
+finally:
+    try:
+        server.quit()
+    except:
+        pass
